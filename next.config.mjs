4df62d086bfac2f7e40ccb5eb8da2e234aa0fbda
abdullahnmp <abdullahnmp@gmail.com>
2025-04-06 21:54:19 +0600

    next.config.mjs updated for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'plus.unsplash.com'],
   },
-  devIndicators: { // এই অংশটি যোগ করুন
+  devIndicators: { 
     staticRouteIndicator: false,
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname), // আপনার প্রকল্পের রুট ডিরেক্টরি
+    };
+    return config;
   },
 }
 
